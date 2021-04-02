4c064c74548b52f39fe48c7b4e696edd97c6cdf9
AdrianRojasAse <74064247+AdrianRojasAse@users.noreply.github.com>
2021-04-02 12:57:45 -0500

    Se incluye manejador de excepciones

---
+++ src/main/java/com/java/demo/ip/Exception/ExceptionHandlerIP.java
@@ -0,0 +1,13 @@
+package com.java.demo.ip.Exception;
+
+public class ExceptionHandlerIP extends Exception {
+
+
+
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionHandlerIP(String message) {
+		super(message);
+	}
+
+}
